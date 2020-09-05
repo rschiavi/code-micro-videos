3929a7b1c125998dd39ce15893aa377ccaaa26a7
Rafael <mrc_rafa@hotmail.com>
2020-09-05 00:19:35 -0300

    exclusao logica de categorias

--- www/app/Models/Category.php
+++ www/app/Models/Category.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     public $fillable = [
         'name', 'description', 'is_active'
+    ];
+
+    public $dates = [
+        'deleted_at'
     ];
 }
